2e0573d1e3dd9fe6c99cce7c98054985ff048ced
Jean-Parra <62663769+Jean-Parra@users.noreply.github.com>
2022-02-25 23:24:42 -0500

    Actualización de front -end

--- src/add-inventory.php
+++ src/add-inventory.php
@@ -8,6 +8,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 ?>
+<html>
     <head>
         <title>Añadir</title>
         <link rel="stylesheet" href="css/containers.css">
@@ -23,34 +24,33 @@ if (!connect()->database->is_ventas($_SESSION["user-id"]) &&
     exit;
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $products = connect()->database->add_inventory($_POST["product_id"],$_POST["product_name"],$_POST["product_amount"],$_POST["product_description"],$_POST["product_price"]);
+if(isset($_POST['add_inventory'])) {
+    $products = connect()->database->add_inventory($_POST["product_name"], $_POST["product_amount"], $_POST["product_description"], $_POST["product_price"]);
+}
     ?>
-    <div class="centered-container">
-        <div class="centered-container-for-input" style="margin-top: 10vh; width: 70vw; height: 70vh;">
-            <h1 class="purple-text" style="margin-top: 10%;">Añadir al inventario</h1>
-            <form method="post">
-                <label>
-                    <input required class="basic-text-input" type="text" placeholder="ID del producto"
-                           name="product_id"
-                           style="width: 75%;">
-                    <input required class="basic-text-input" type="text" placeholder="Nombre del producto"
-                           name="product_name"
-                           style="width: 75%;">
-                    <input required class="basic-text-input" type="text" placeholder="Cantidad del producto"
-                           name="product_amount"
-                           style="width: 75%;">
-                    <input required class="basic-text-input" type="text" placeholder="Descripcion del producto"
-                           name="product_description"
-                           style="width: 75%;">
-                    <input required class="basic-text-input" type="text" placeholder="Precio del producto"
-                           name="product_price"
-                           style="width: 75%;">
-                </label>
-                <br>
-                <button class="blue-button" type="submit" style="margin-top: 5%; width: 75%;">Añadir</button>
-            </form>
+    <body>
+        <div class="centered-container">
+            <div class="centered-container-for-input" style="margin-top: 10vh; width: 70vw; height: 70vh;">
+                <h1 class="purple-text" style="margin-top: 10%;">Añadir al inventario</h1>
+                <form method="post">
+                    <label>
+                        <input required class="basic-text-input" type="text" placeholder="Nombre del producto"
+                               name="product_name"
+                               style="width: 75%;">
+                        <input required class="basic-text-input" type="text" placeholder="Cantidad del producto"
+                               name="product_amount"
+                               style="width: 75%;">
+                        <input required class="basic-text-input" type="text" placeholder="Descripcion del producto"
+                               name="product_description"
+                               style="width: 75%;">
+                        <input required class="basic-text-input" type="text" placeholder="Precio del producto"
+                               name="product_price"
+                               style="width: 75%;">
+                    </label>
+                    <br>
+                    <button class="blue-button" type="submit" name="add_inventory" style="margin-top: 5%; width: 75%;">Añadir</button>
+                </form>
+            </div>
         </div>
-    </div>
-    <?php
+    </body>
 }
