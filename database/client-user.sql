7730414bd6c4be9ee51aff8634aea075cb1c8755
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2022-03-24 13:43:47 -0500

    Mas permisos

--- database/client-user.sql
+++ database/client-user.sql
@@ -16,6 +16,13 @@ GRANT EXECUTE ON FUNCTION upb_motors.registrar_orden TO 'upb_motors_client'@'loc
 GRANT EXECUTE ON FUNCTION upb_motors.actualizar_cantidad_orden TO 'upb_motors_client'@'localhost';
 GRANT EXECUTE ON FUNCTION upb_motors.close_factura TO 'upb_motors_client'@'localhost';
 GRANT EXECUTE ON FUNCTION upb_motors.close_orden TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.registrar_factura TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.registrar_detalles_factura TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.registrar_gasto TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.cancel_purchase TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.get_dependencia_name TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.get_dependencia_id TO 'upb_motors_client'@'localhost';
+GRANT EXECUTE ON FUNCTION upb_motors.registrar_detalles_orden TO 'upb_motors_client'@'localhost';
 
 GRANT SELECT ON upb_motors.* TO 'upb_motors_client'@'localhost';
 GRANT INSERT ON upb_motors.* TO 'upb_motors_client'@'localhost';
