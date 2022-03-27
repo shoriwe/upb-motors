367fe8b5370a0508df1c8a135ecbffc7e8965960
Antonio <antoniojosedonishung@gmail.com>
2022-03-27 14:36:43 -0500

    Removida la clase de pruebas de la base de datos

--- src/connection.php
+++ src/connection.php
@@ -19,22 +19,18 @@ function connect(): Connection
 {
     if (!isset($_SERVER["connection"])) {
         $_SERVER["connection"] = new Connection();
-        if (getenv("TEST") !== false) {
-            $_SERVER["connection"]->database = new TestDatabase();
-            $_SERVER["connection"]->email = new TestEmail();
-        } else {
-            $_SERVER["connection"]->database = new MySQL(
-                getenv("DB_HOST"),
-                getenv("DB_USERNAME"),
-                getenv("DB_PASSWORD"),
-                getenv("DB_DATABASE"));
-            $_SERVER["connection"]->email = new SMTPEmail(
-                getenv("EMAIL_HOST"),
-                intval(getenv("EMAIL_PORT")),
-                getenv("EMAIL_USERNAME"),
-                getenv("EMAIL_PASSWORD")
-            );
-        }
+        $_SERVER["connection"]->database = new MySQL(
+            getenv("DB_HOST"),
+            getenv("DB_USERNAME"),
+            getenv("DB_PASSWORD"),
+            getenv("DB_DATABASE"));
+        $_SERVER["connection"]->email = new SMTPEmail(
+            getenv("EMAIL_HOST"),
+            intval(getenv("EMAIL_PORT")),
+            getenv("EMAIL_USERNAME"),
+            getenv("EMAIL_PASSWORD")
+        );
+
     }
     return $_SERVER["connection"];
 }
